571410479a6db0ede07388aa8a78f8f5b4abaae0
Sarvarbek2003 <sarvarbekodilov070603@gmail.com>
2023-10-17 21:12:59 +0500

    push

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import TelegramBot from 'node-telegram-bot-api'
-import { home } from './menu/static-menu'
 import { renderCategory, renderSubcategory } from './menu/dinamic.menu'
+import { Chanell, PrismaClient, Users } from "@prisma/client"
+import { getChatMember, getUser } from './user/users';
+const { convert } = require('convert-svg-to-png');
+import TelegramBot from "node-telegram-bot-api"
+import { home } from './menu/static-menu'
+import svgCaptcha from 'svg-captcha';
 
+const prisma = new PrismaClient()
 const bot = new TelegramBot('5520485548:AAEfXoTQLG0nDPEhA1pBnv_B9NsqF8PaHRY', {polling: true})
+
 
 bot.on('text', async msg => {
     const chat_id = msg.from!.id
     const text = msg.text
+    const { user, new_user } = await getUser(msg)
 
     if(text == '/start') {
         bot.sendMessage(chat_id, "Ассалому Алайкум.\nСўровнома ботга ҳуш келибсиз!", {
@@ -16,13 +23,23 @@ bot.on('text', async msg => {
         bot.sendMessage(chat_id, "Қуйидаги кўрсатилган сўровномалардан бирини танланг 👇", {
             reply_markup: await renderCategory()
         })
+    } else if(user.step == 'captcha') {
+
+        if(Object(user.action).captcha == text) {
+
+            bot.sendMessage(chat_id,  "To'gri" )
+                
+        } else { 
+            
+            bot.sendMessage(chat_id,  "no To'gri" )
+        }
     }
 })
 
 bot.on('callback_query', async msg => {
     let data = msg.data
     const chat_id = msg.from.id
-
+    const { user, new_user } = await getUser(msg)
     let text = data?.split(':')[0]
     data = data?.split(':')[1]
     
@@ -34,7 +51,35 @@ bot.on('callback_query', async msg => {
             reply_markup: keyboard
         })
     } else if (text == 'subcategory') {
+        let { is_member, not_members } = await getChatMember(bot, msg)
+
+        if(!is_member) {
+            let keyboard = not_members.map((el, index) => {
+                return [{text: index + 1 +' каналга азо бўлиш', url: 'https://t.me/'+ el.chanell_username}]
+            })
+            return bot.sendMessage(chat_id, `❗️Илтимос, сўровномада иштирок этиш учун қуйидаги ${not_members.length} та каналга аъзо бўлинг.`, {
+                reply_markup: {
+                    inline_keyboard: keyboard
+                }   
+            })
+        }
+
         bot.deleteMessage(chat_id, msg.message!.message_id)
-        bot.sendMessage(chat_id, "")
+        const captcha = svgCaptcha.createMathExpr({mathMin: 1 + Math.random() * 9 | 0, mathMax: 1 + Math.random() * 9 | 0, width: 200, height: 100});
+        
+        const pngBuffer = await convert(captcha.data, {
+            puppeteer: { args: ['--no-sandbox'] },
+        });
+
+        user.action = {
+            captcha: captcha.text
+        }
+
+        await prisma.users.update({where: {chat_id: chat_id}, data: {action: user.action, step: 'captcha'}})
+        bot.sendPhoto(chat_id, pngBuffer, {
+            caption: 'Амални бажаринг'
+        })
     }
 })
+
+
---
+++ src/user/users.ts
@@ -0,0 +1,54 @@
+import { Chanell, PrismaClient, Users } from "@prisma/client"
+import TelegramBot from "node-telegram-bot-api"
+const prisma = new PrismaClient()
+
+const getUser = async (msg: TelegramBot.Message | TelegramBot.CallbackQuery ):Promise<{user:Users, new_user:boolean}> => {
+
+    let chat_id:TelegramBot.ChatId = msg.from!.id 
+    let is_user:Users | null  = await prisma.users.findUnique({where: {chat_id}})
+
+    if (!is_user) {
+        let new_user = await prisma.users.create({
+            data:{
+                chat_id,
+                name: msg!.from?.first_name || '',
+                action: {}
+            }
+        })
+        return {user: new_user, new_user: true}
+    } 
+
+    return {
+        user: is_user, 
+        new_user: false
+    }
+}
+
+const getChatMember = async (bot:TelegramBot, msg:TelegramBot.Message | TelegramBot.CallbackQuery): Promise<{is_member:boolean, not_members: Array<Chanell>}> => {
+    try {
+        let chanells:Chanell[] | [] = await prisma.chanell.findMany()
+        
+        if(!chanells.length) return {is_member:false, not_members: []}
+
+        let is_notMembers:Chanell[] = []
+        for (const chanell of chanells) {
+            let user = await bot.getChatMember(chanell!.chanell_id!, msg.from!.id)
+            if (!['member', 'administrator', 'creator'].includes(user.status)){
+                is_notMembers.push(chanell)
+            }
+        }
+        
+        if(is_notMembers.length) {
+            return {is_member:false, not_members: is_notMembers}
+        }
+        
+        return {is_member:true, not_members: is_notMembers}
+    } catch (error) {
+        console.log(error);
+        
+        return {is_member:true, not_members: []}
+    }
+
+}
+
+export { getUser, getChatMember }
